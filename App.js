a46e60b12546aa8606d074086c7ccb698f023378
valentineejk <valentineejk@gmail.com>
2022-04-19 23:12:21 +0100

    restaurant card component

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image,StyleSheet,Text,View} from 'react-native';
+import RestaurantItem from './src/components/RestaurantItem';
+import restaurants from './assets/data/restaurants.json'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RestaurantItem
+        restaurant={restaurants[0]}
+      />
+      <RestaurantItem
+        restaurant={restaurants[3]}
+      />
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+import React from 'react'
+
+
+
+const styles=StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
+
 });
---
+++ src/components/RestaurantItem.js
@@ -0,0 +1,45 @@
+import {Image,StyleSheet,Text,View} from 'react-native';
+
+
+
+
+const RestaurantItem=({restaurant}) => {
+    return (
+        <View style={styles.restaurantsContainer}>
+            <Image
+                source={{uri: restaurant.image}}
+                style={styles.image}
+            />
+            <Text style={styles.title}>
+                {restaurant.name}
+            </Text>
+            <Text style={styles.subtitle}>
+                $ {restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes
+            </Text>
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+
+    restaurantsContainer: {
+        width: '100%',
+        marginVertical: 10,
+    },
+    image: {
+        width: "100%",
+        aspectRatio: 5/3,
+        marginBottom: 5,
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: "500",
+        marginVertical: 5,
+    },
+    subtitle: {
+        color: "grey",
+    }
+});
+
+
+export default RestaurantItem;
